516ff356854d541e1ab6ec01b64e11aa7da43b4e
Nazar Oshchepkov <nazzarin565@gmail.com>
2024-08-22 17:35:04 +0300

    make build

---
+++ index.82ab3a6c.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("table"),t=e.querySelector("thead tr"),r=e.querySelectorAll("tbody tr"),l=e.querySelector("tfoot tr"),o=t.querySelectorAll("th"),c=o.item(1).cloneNode(!0);t.insertBefore(c,o[o.length-1]),r.forEach(function(e){var t=e.querySelectorAll("td"),r=t.item(1).cloneNode(!0);e.insertBefore(r,t[t.length-1])});var n=l.querySelectorAll("th"),u=n.item(1).cloneNode(!0);l.insertBefore(u,n[n.length-1]);
+//# sourceMappingURL=index.82ab3a6c.js.map
